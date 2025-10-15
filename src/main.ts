958c5b595aedfed3b7d16b5ad5093e9d714929d1
ninza-innovates <zkctuepf101@gmx.com>
2025-10-15 19:55:39 +0200

    fix: remove the dscription

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,17 @@ import { createApp } from 'vue'
 import '@/styles/styles.scss'
 import App from './App.vue'
 
-// Vue-Router
 import router from '@/plugins/router'
 
-// Vuetify
 import vuetify from '@/plugins/vuetify'
 
-// Vue-i18n
 import i18n from '@/plugins/i18n'
 
-// Vue-Toastify
 import Vue3Toastify from 'vue3-toastify'
 import options from '@/plugins/toastify'
 
-// Pinia
 import pinia from '@/plugins/pinia'
 
-// Font
 import '@fontsource/roboto'
 
 const app = createApp(App)
